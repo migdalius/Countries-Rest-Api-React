4b1fcaa76322fa44401586f7145af68d5ff5e05d
migdalius <30595371+migdalius@users.noreply.github.com>
2022-12-19 18:06:18 +0100

    toggle part 1

--- src/pages/home/Home.jsx
+++ src/pages/home/Home.jsx
@@ -1,11 +1,12 @@
 import Flags from "../../components/flags/Flags";
 import Hero from "../../components/hero/Hero";
 import Nav from "../../components/nav/Nav";
+
 import "../home/home.css";
 
 const Home = () => {
   return (
-    <div>
+    <div style={{ backgroundColor: "black", color: "black" }}>
       <Nav />
       <Hero />
       <Flags />
